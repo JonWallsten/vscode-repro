faabfa0d50081399591a298b8587d4107ed9475b
Jon Wallsten <jon.wallsten@gmail.com>
2021-11-09 17:08:09 +0100

    Initial commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+import { double } from '..';
+import { power } from './number';
+
+export function doubleNumber() {
+    return double(2);
+}
+
+export function powerNumber() {
+    return power(2, 2);
+}
